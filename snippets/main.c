53001de236deeb53ed26b0a72dbf24b39a849b08
bem22 <be.mihai22@gmail.com>
2021-01-27 19:18:08 +0100

    GVariants and iterators

--- snippets/main.c
+++ snippets/main.c
@@ -1,98 +1,60 @@
-
-#include <string.h>
-
 #include <glib.h>
 #include <gio/gio.h>
 
-GDBusConnection *con;
-static const gchar* bluez_adapter_get_property(const gchar *path, const char *name)
-{
-    GVariant *result;
-    GVariant *prop;
-    GError *error = NULL;
-    const gchar *address = NULL;
+int main(void) {
+    GError *err = NULL;
+    GDBusConnection *conn = g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, &err);
 
-    result = g_dbus_connection_call_sync(con,
-                                         "org.bluez",
-                                         path,
-                                         "org.freedesktop.DBus.Properties",
-                                         "Get",
-                                         g_variant_new("(ss)", "org.bluez.Adapter1", name),
-                                         NULL,
-                                         G_DBUS_CALL_FLAGS_NONE,
-                                         -1,
-                                         NULL,
-                                         &error);
-    if(error != NULL)
-        return NULL;
-
-    g_variant_get(result, "(v)", &prop);
-    g_variant_unref(result);
-    return g_variant_get_string(prop, NULL);
-}
-
-static void bluez_list_controllers(GDBusConnection *con,
-                                   GAsyncResult *res,
-                                   gpointer data)
-{
-    GVariant *result = NULL;
-    GMainLoop *loop = NULL;
-    gchar *s = NULL;
-    GVariantIter i;
-    const gchar *object_path;
-    GVariant *ifaces_and_properties;
-
-    loop = (GMainLoop *)data;
-    result = g_dbus_connection_call_finish(con, res, NULL);
-    if(result == NULL)
-        g_print("Unable to get result for GetManagedObjects\n");
-
-    /* Parse the result */
-    if(result) {
-        result = g_variant_get_child_value(result, 0);
-        g_variant_iter_init(&i, result);
-        while(g_variant_iter_next(&i, "{&o@a{sa{sv}}}", &object_path, &ifaces_and_properties)) {
-            const gchar *interface_name;
-            GVariant *properties;
-            GVariantIter ii;
-            g_variant_iter_init(&ii, ifaces_and_properties);
-            while(g_variant_iter_next(&ii, "{&s@a{sv}}", &interface_name, &properties)) {
-                if(g_strstr_len(g_ascii_strdown(interface_name, -1), -1, "adapter"))
-                    g_print("HCI Name: %s Address: %s\n", bluez_adapter_get_property(object_path, "Name"), bluez_adapter_get_property(object_path, "Address"));
-                g_variant_unref(properties);
-            }
-            g_variant_unref(ifaces_and_properties);
-        }
-        g_variant_unref(result);
-    }
-    g_main_loop_quit(loop);
-}
-
-int main(void)
-{
-    GMainLoop *loop;
-
-    con = g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, NULL);
-    if(con == NULL) {
-        g_print("Not able to get connection to system bus\n");
+    if(err != NULL) {
+        g_print("%s\n%s\n", "Error while attempting to establish a connection with DBUS", err->message);
         return 1;
+    } else {
+        g_print("\n%s\n", "Connection to DBUS successful!");
     }
 
-    loop = g_main_loop_new(NULL, FALSE);
-    g_dbus_connection_call(con,
-                           "org.bluez",
-                           "/",
-                           "org.freedesktop.DBus.ObjectManager",
-                           "GetManagedObjects",
-                           NULL,
-                           G_VARIANT_TYPE("(a{oa{sa{sv}}})"),
-                           G_DBUS_CALL_FLAGS_NONE,
-                           -1,
-                           NULL,
-                           (GAsyncReadyCallback)bluez_list_controllers,
-                           loop);
+    GDBusProxy *proxy = g_dbus_proxy_new_sync(conn,
+                                              G_DBUS_PROXY_FLAGS_NONE,
+                                              NULL,
+                                              "org.bluez",
+                                              "/",
+                                              "org.freedesktop.DBus.ObjectManager",
+                                              NULL,
+                                              &err);
 
-    g_main_loop_run(loop);
-    g_object_unref(con);
-    return 0;
+    if(err != NULL) {
+        g_print("%s\n%s\n", "Error while attempting to create a proxy", err->message);
+        return 1;
+    } else {
+        g_print("\n%s\n", "Proxy created successfully!");
+    }
+
+    GVariant *result = g_dbus_proxy_call_sync(proxy,
+                                              "GetManagedObjects",
+                                              NULL,
+                                              G_DBUS_CALL_FLAGS_NONE,
+                                              -1,
+                                              NULL,
+                                              &err);
+    if(err != NULL) {
+        g_print("%s\n%s\n", "Error while fetching data", err->message);
+        return 1;
+    } else {
+        g_print("\n%s\n", "Data fetched successfully");
+    }
+
+    if(result) {
+        g_variant_get_child_value(result, 0);
+        GVariantIter iter;
+        g_variant_iter_init(&iter, result);
+
+        int i=0;
+        while(g_variant_iter_next(&iter, "a{oa{sa{sv}}}", NULL)) {
+            i++;
+        }
+
+        g_print("%i", i);
+    }
+
+
+
 }
