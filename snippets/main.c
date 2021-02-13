5ce86b379911b41285951b8802eb39217d8d8eda
bem22 <be.mihai22@gmail.com>
2021-02-13 02:53:41 +0100

    StartNotify version
    TO-DO: find out where the notifications are sent!

--- snippets/main.c
+++ snippets/main.c
@@ -1,6 +1,7 @@
 #include <glib.h>
 #include <gio/gio.h>
 #include <stdio.h>
+#include <pthread.h>
 
 void *handle_ble_connection() {
     while(1) {
@@ -11,17 +12,26 @@ void *handle_ble_connection() {
 
 }
 
+static void  hello_world(GDBusProxy *proxy, GAsyncResult *res) {
+    GVariant *result = NULL;
+    GError *err = NULL;
+    result = g_dbus_proxy_call_finish(proxy, res, &err);
+
+    if(err != NULL) {
+        g_print("%s\n%s\n", "Error while attempting to establish a connection with DBUS", err->message);
+
+    } else {
+        g_print("\n%s %s\n", "Connection to DBUS successful: ", g_variant_get_type_string(result));
+    }
+
+}
+
 int main(void) {
-    pthread_t *ble_conn_watcher = (pthread_t*) malloc(sizeof(pthread_t));
-
-    // Start the thread that watches over interfaces
-    pthread_create(ble_conn_watcher, NULL, &handle_ble_connection, NULL);
-
-    pthread_join(*ble_conn_watcher, NULL);
-
     GError *err = NULL;
     GDBusConnection *conn = g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, &err);
 
+    GMainLoop *loop;
+    loop = g_main_loop_new(NULL, FALSE);
     if(err != NULL) {
         g_print("%s\n%s\n", "Error while attempting to establish a connection with DBUS", err->message);
         return 1;
@@ -77,7 +87,63 @@ int main(void) {
         g_print("\n%s\n", "Proxy created successfully!");
     }
 
+    for(int i=0; i<15; i++) {
+        GVariantBuilder builder;
+        g_variant_builder_init (&builder, G_VARIANT_TYPE("a{sv}"));
+        GVariant *argument = g_variant_builder_end(&builder);
 
+        char* data = "1";
+        if(i%2==0) {
+            data = "";
+        } else {data = "1";}
+        GVariant *char_value = g_variant_new_from_data(G_VARIANT_TYPE("ay"), data, 1, TRUE, NULL, NULL);
+
+
+        GVariant *result = g_dbus_proxy_call_sync(gatt_proxy,
+                                                  "WriteValue",
+                                                  g_variant_new("(@ay@a{sv})", char_value, argument),
+                                                  G_DBUS_CALL_FLAGS_NONE,
+                                                  -1,
+                                                  NULL,
+                                                  &err);
+        if(err != NULL) {
+            g_print("%s\n%s\n", "Error while calling a method through proxy", err->message);
+            return 1;
+        } else {
+            g_print("\n%s\n", "Proxy method call, successful!");
+        }
+
+        usleep(100);
+    }
+
+    GDBusProxy *gatt_proxy2 = g_dbus_proxy_new_sync(conn,
+                                                   G_DBUS_PROXY_FLAGS_NONE,
+                                                   NULL,
+                                                   "org.bluez",
+                                                   "/org/bluez/hci0/dev_DC_3F_32_42_62_B0/service0018/char0022",
+                                                   "org.bluez.GattCharacteristic1",
+                                                   NULL,
+                                                   &err);
+
+    g_dbus_proxy_call(gatt_proxy2,
+                           "StartNotify",
+                           NULL,
+                           G_DBUS_CALL_FLAGS_NONE,
+                           -1,
+                           NULL,
+                           hello_world,
+                           NULL);
+    if(err != NULL) {
+        g_print("%s\n%s\n", "Error while starting notify", err->message);
+        return 1;
+    } else {
+        g_print("\n%s\n", "Notify started!");
+    }
+
+
+
+    g_main_loop_run(loop);
+    g_object_unref(conn);
 
 
 }
